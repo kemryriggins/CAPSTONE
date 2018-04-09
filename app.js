ab48502f6ec8539be367f2841bad81ae0fff2a8a
Kemry Riggins <98kariggins@gmail.com>
2018-04-09 17:09:06 +0000

    changed Landing and campgrounds layouts

--- app.js
+++ app.js
@@ -6,34 +6,33 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-        {name: "Salmon Creek", image: "https://www.nps.gov/zion/planyourvisit/south-campground.htm"},
-        {name: "Zions ", image: "https://www.allzion.com/rv_camping/"},
-        {name: "Yosemite", image: "https://jameskaiser.com/yosemite-guide/camping-near-yosemite/"},
-]
-        
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+];
+    
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-     res.render("campgrounds",{campgrounds:campgrounds});
+    res.render("campgrounds",{campgrounds:campgrounds});
 });
 
 app.post("/campgrounds", function(req, res){
-    res.send("YOU HIT THE POST ROUTE!")
-    // get data form and add to campgrounds array
+    // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image}
     campgrounds.push(newCampground);
-    // redirect back to campgrounds page
+    //redirect back to campgrounds page
     res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req, res){
-    res.render("new.ejs");
+   res.render("new.ejs"); 
 });
 
-app.listen(process.env.PORT, process.env.IP, function (){
-    console.log("The Campout Server Has Started");
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
 });
